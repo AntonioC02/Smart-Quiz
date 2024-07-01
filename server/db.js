b964f22b5afec757250b2ac921b70ff32682b09d
AntonioC02 <120738475+AntonioC02@users.noreply.github.com>
2024-07-01 20:16:48 +0200

    other tests2

--- server/db.js
+++ server/db.js
@@ -3,10 +3,10 @@ import pg from 'pg';
 const { Pool } = pg;
 
 const pool = new Pool({
-    user: 'postagres_owner',
-    host: 'ep-hidden-poetry-a2i3vdr0-pooler.eu-central-1.aws.neon.tech',
-    database: 'postagres',
-    password: '9nNrQSmlPI6i',
+    user: 'postgres',
+    host: 'localhost',
+    database: 'postgres',
+    password: 'pwd',
     port: 5432,
 });
 
